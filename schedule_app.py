572181b21e5da80de162423e24ac2c6ab412d7c4
edhio93 <edgar.hidalgo.o@colegioantoniovaras.cl>
2026-04-10 10:16:35 -0400

    Update schedule_app.py

--- schedule_app.py
+++ schedule_app.py
@@ -235,7 +235,7 @@ if os.path.exists(archivo_alerta):
 
                 # 3. Frenamos el código aquí para que no dibuje el resto del colegio de fondo
                 st.stop() 
-            else:
+          else:
                 # Si ya pasó la hora límite, la borramos automáticamente
                 os.remove(archivo_alerta)
         except Exception as e:
